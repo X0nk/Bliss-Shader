11f0717252f6644b01c4236809b6893ce6135484
Xonk <XonkDev@gmail.com>
2025-09-06 22:19:43 -0400

    make reinhard_jodie_ruined tonemap setting. make reflections submenu a single column

--- shaders/lib/settings.glsl
+++ shaders/lib/settings.glsl
@@ -530,7 +530,7 @@ uniform int moonPhase;
 // ----- COLOR/POST PROCESSING RELATED SETTINGS ----- //
 ////////////////////////////////////////////////////////
 
-#define TONEMAP ToneMap_AgX_minimal // [no_tonemapper reinhard_jodie ToneMap_AgX_minimal ToneMap_AgX ToneMap_Hejl2015 Tonemap_Xonk Tonemap_Uchimura HableTonemap Full_Reinhard_Edit Tonemap_Full_Reinhard reinhard Tonemap_Lottes ACESFilm]
+#define TONEMAP ToneMap_AgX_minimal // [no_tonemapper reinhard_jodie_ruined reinhard_jodie ToneMap_AgX_minimal ToneMap_AgX ToneMap_Hejl2015 Tonemap_Xonk Tonemap_Uchimura HableTonemap Full_Reinhard_Edit Tonemap_Full_Reinhard reinhard Tonemap_Lottes ACESFilm]
 //#define USE_ACES_COLORSPACE_APPROXIMATION
 
 #define CONTRAST_ADAPTATIVE_SHARPENING
