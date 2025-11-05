0912bdac6dab0f2eb908027a47707846ed31c1c2
Xonk <XonkDev@gmail.com>
2025-11-04 22:53:34 -0500

    fix horizon speckles with altostratus clouds

--- shaders/lib/volumetricClouds.glsl
+++ shaders/lib/volumetricClouds.glsl
@@ -281,7 +281,7 @@ vec4 raymarchCloud(
 	if(LayerIndex == ALTOSTRATUS_LAYER){
 		float density = parameters.altostratus.y;
 
-		bool ifAboveOrBelowPlane = max(mix(-1.0, 1.0, clamp(cameraPosition.y - minHeight,0.0,1.0)) * normalize(rayDirection).y,0.0) > 0.0;
+		bool ifAboveOrBelowPlane = max(mix(-1.0, 1.0, clamp(cameraPosition.y - minHeight,0.0,1.0)) * normalize(rayDirection).y + 0.0001,0.0) > 0.0;
 
 		// check if the ray staring position is going farther than the reference distance, if yes, dont begin marching. this is to check for intersections with the world.
 		// check if the camera is above or below the cloud plane, so it doesnt waste work on the opposite hemisphere
