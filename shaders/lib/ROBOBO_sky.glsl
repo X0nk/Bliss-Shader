e9ec273106f2ef33efa1a9b9afaa49651143f01d
Xonk <XonkDev@gmail.com>
2025-11-13 21:50:37 -0500

    slightly tweak sky

--- shaders/lib/ROBOBO_sky.glsl
+++ shaders/lib/ROBOBO_sky.glsl
@@ -103,17 +103,29 @@ vec3 calculateAtmosphere(vec3 background, vec3 viewVector, vec3 upVector, vec3 s
 	
 	float GroundDarkening = max(planetGround * 0.7+0.3,clamp(sunVector.y*2.0,0.0,1.0));
 
-	vec3 viewPos = (sky_planetRadius + eyeAltitude) * upVector;
+	vec3 viewPos = (sky_planetRadius + 1.0 + max(eyeAltitude-300.0,0.0)*1.0) * upVector;
 
 	vec2 aid = rsi(viewPos, viewVector, sky_atmosphereRadius);
 	if (aid.y < 0.0) {transmittance = vec3(1.0); return vec3(0.0);}
 
 	pid = rsi(viewPos, viewVector, sky_planetRadius * 0.998);
-	bool planetIntersected = pid.y >= 0.0;
+	bool planetIntersected = pid.y > 0.0;
 
 	vec2 sd = vec2((planetIntersected && pid.x < 0.0) ? pid.y : max(aid.x, 0.0), (planetIntersected && pid.x > 0.0) ? pid.x : aid.y);
+// 	vec2 sd = vec2(
+// (planetIntersected && pid.x < 0.0) ? pid.y : clamp(aid.x,0.0,99999.9),
+// (planetIntersected && pid.x > 0.0) ? pid.x : clamp(aid.y,0.0,99999.9)
+// );
+//  vec2 sd = vec2(
+// 	(planetIntersected && pid.x < 0.0) ? pid.y : clamp(aid.x,0.0,99999.9),
+//  	(planetIntersected && pid.x > 0.0) ? pid.x : clamp(aid.y,0.0,99999.9)
+// );
+//  vec2 sd = vec2(
+// 	clamp(aid.x,0.0,99999.9),
+//  	clamp(aid.y,0.0,99999.9)
+// );
 
-	float stepSize  = (sd.y - sd.x) * (1.0 / iSteps);
+	float stepSize  = (sd.y - sd.x) * (1.0 /iSteps);
 	vec3  increment = viewVector * stepSize;
 	vec3  position  = viewVector * sd.x + viewPos;
 	position += increment * (0.34*noise);
@@ -131,26 +143,27 @@ vec3 calculateAtmosphere(vec3 background, vec3 viewVector, vec3 upVector, vec3 s
 	float low_sun = clamp(((1.0-abs(sunVector.y))*3.) - high_sun,1.0,2.0) ;
 	
 	#if defined OVERWORLD_SHADER && defined TWILIGHT_FOREST_FLAG
-		low_sun = 1.5;
+		low_sun = 2.5;
 	#endif
 
 	for (int i = 0; i < iSteps; ++i, position += increment) {
-		vec3 density = sky_density(length(position));
+		vec3 density = sky_density(length(position)) ;
 		if (density.y > 1e35) break;
+
 		vec3 stepAirmass      = density * stepSize ;
-		vec3 stepOpticalDepth = sky_coefficientsAttenuation * stepAirmass ;
+		vec3 stepOpticalDepth = sky_coefficientsAttenuation * stepAirmass;
 
 		vec3 stepTransmittance       = exp2(-stepOpticalDepth * rLOG2);
 		vec3 stepTransmittedFraction = clamp01((stepTransmittance - 1.0) / -stepOpticalDepth) ;
-		vec3 stepScatteringVisible   = transmittance * stepTransmittedFraction * GroundDarkening ;
+		vec3 stepScatteringVisible   = transmittance * stepTransmittedFraction ;
 		
 		#ifdef ORIGINAL_CHOCAPIC_SKY
-			scatteringSun  += sky_coefficientsScattering  * (stepAirmass.xy * phaseSun) * stepScatteringVisible * sky_transmittance(position, sunVector,  jSteps) * planetGround;
+			scatteringSun  += sky_coefficientsScattering  * (stepAirmass.xy * phaseSun) * stepScatteringVisible * sky_transmittance(position, sunVector,  jSteps) ;
 		#else
-			scatteringSun  += sky_coefficientsScattering  * (stepAirmass.xy * phaseSun) * stepScatteringVisible * sky_transmittance(position, sunVector,  jSteps) * planetGround;
+			scatteringSun  += sky_coefficientsScattering  * (stepAirmass.xy * phaseSun) * stepScatteringVisible * sky_transmittance(position, sunVector,  jSteps) *planetGround;
 		#endif
 
-		scatteringMoon += sky_coefficientsScattering * (stepAirmass.xy * phaseMoon) * stepScatteringVisible * sky_transmittance(position, moonVector, jSteps) * planetGround;
+		scatteringMoon += sky_coefficientsScattering * (stepAirmass.xy * phaseMoon) * stepScatteringVisible * sky_transmittance(position, moonVector, jSteps) ;
 		
 		// Nice way to fake multiple scattering.
 		#ifdef ORIGINAL_CHOCAPIC_SKY
@@ -162,7 +175,7 @@ vec3 calculateAtmosphere(vec3 background, vec3 viewVector, vec3 upVector, vec3 s
 		transmittance *= stepTransmittance;
 	}
 	
-	vec3 scattering = scatteringAmbient * background + scatteringSun * sunColorBase + scatteringMoon*moonColorBase * 0.5;
+	vec3 scattering = scatteringAmbient * background+ scatteringSun * sunColorBase + scatteringMoon*moonColorBase * 0.5;
 
 	return scattering;
 }
