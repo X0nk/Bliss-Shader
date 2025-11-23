36739e3d671b57b6ae8eb90bb4da340bce18cca2
Xonk <XonkDev@gmail.com>
2025-11-22 21:28:50 -0500

    disable seperate_entity_draws by default

--- shaders/lib/settings.glsl
+++ shaders/lib/settings.glsl
@@ -707,7 +707,7 @@ const vec3 aerochrome_color = mix(vec3(1.0, 0.0, 0.0), vec3(0.715, 0.303, 0.631)
 #define RESOURCEPACK_SKY 0 // [0 1 2 3]
 
 #ifdef IS_IRIS
-	#define TRANSLUCENT_ENTITIES
+	// #define TRANSLUCENT_ENTITIES
 #endif
 
 #define WATER_CAUSTICS_BRIGHTNESS 1.0 // [0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2.0 3.0 4.0 5.0 6.0 7.0 8.0 9.0 10.0]
